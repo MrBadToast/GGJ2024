98687b17129d2ae0faaf00d6cf6d10c020e2853b
Victor <fatiger92@gmail.com>
2024-01-28 06:53:01 +0900

    해상도 FHD 고정

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -8,8 +8,22 @@ public class UIManager : MonoBehaviour
     public static UIManager instance;
     private void Awake() => instance = this;
 
+    // 원하는 해상도 설정
+    public int targetScreenWidth = 1920;
+    public int targetScreenHeight = 1080;
+    
     public ComedySelectWindow comedySelectWindow;
     public MiniGameWindow miniGameWindow;
     public ResultWindow resultWindow;
     
+    void Start()
+    {
+        // 원하는 해상도로 설정
+        SetResolution(targetScreenWidth, targetScreenHeight);
+    }
+
+    void SetResolution(int width, int height)
+    {
+        Screen.SetResolution(width, height, true);
+    }
 }
