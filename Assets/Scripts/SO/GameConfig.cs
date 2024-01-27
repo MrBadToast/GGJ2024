c67482e0671f8db70e5bee703359db7917d6962f
BadToast <ship2042@gmail.com>
2024-01-27 20:22:36 +0900

    MaingameResources

--- Assets/Scripts/SO/GameConfig.cs
+++ Assets/Scripts/SO/GameConfig.cs
@@ -27,15 +27,6 @@ public class GameConfig : ScriptableObject
         public Level Name;
         public int CompletePoint;
 
-        //========= obsolete =========
-
-        public string OptionHint;
-        public string OptionAnswer;
-        public string OptionWrongAnwser_1;
-        public string OptionWrongAnwser_2;
-
-        //========= obsolete =========
-
         public int DefaultPoint;
         public int WrongAnswerTimes;
         public int AnswerBonusTimes;
